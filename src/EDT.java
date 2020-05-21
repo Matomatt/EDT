3530dfd41754c568fd66ffe1ba077350e04ab489
LeonieCdt <leonie.zoi@hotmail.fr>
2020-05-21 17:33:18 +0200

    _hht

--- src/EDT.java
+++ src/EDT.java
@@ -28,11 +28,6 @@ public class EDT {
 	
 	public static void main(String args[])
 	{
-            
-        Login_frame page = new Login_frame();
-        page.setVisible(true);
-        
-        
         User user = null;
         try {
         	user = new ConnectionViaUser("admin", "pw");
