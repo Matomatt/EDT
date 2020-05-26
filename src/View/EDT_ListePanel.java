f51f3bf4e7d0dc0923a8d49133b931dc413e9cc9
LeonieCdt <leonie.zoi@hotmail.fr>
2020-05-26 19:22:24 +0200

    Debut implementation liste dans modifAdmin

--- src/View/EDT_ListePanel.java
+++ src/View/EDT_ListePanel.java
@@ -8,29 +8,58 @@ package View;
 import Seances.Seance;
 import java.util.List;
 import UI_Elements.Button;
+import UI_Elements.JScrollListe;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import Utilitaires.ImageManager;
+
+import javax.swing.AbstractListModel;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JToolBar;
+import javax.swing.SwingConstants;
 
-import Controllers.Controller;
 import Utilisateurs.User;
+import static java.util.Collections.list;
 import javax.swing.JTable;
 
 /**
  *
  * @author Léonie
  */
-public class EDT_ListePanel extends Panel
+public class EDT_ListePanel extends JPanel
 {    
     private static final long serialVersionUID = 3736956335101565252L;
+    User user = null;
 
-    public EDT_ListePanel(User _user, Controller controller) 
+    public EDT_ListePanel(User _user) 
     {
-    	super(_user, controller);
+	user = _user;
+	this.setLayout(new GridBagLayout());		
 		
         initComponents();//code de la page
+        
+        this.setVisible(true);
+        
+        validate();
     }
 	
     private void initComponents() 
@@ -42,28 +71,32 @@ public class EDT_ListePanel extends Panel
 
         System.out.println("toto");
         
-       for (int y=0; y<6;y++)
+       /*for (int y=0; y<6;y++)
        {
            
-       }
+       }*/
+       
+       //on récupère une liste avec toutes les séances qui se passe pendant une journée.
+       List<Seance> liste = user.ListeSeances().getByUtilisateurAtDate(user.ListeUtilisateurs().getByID(1709), "2020-05-26");
+       //on récupère la taille pour savoir le nombre de cours en une journée et pouvoir ensuite passer au jour d'après.
+       int taille = liste.size();
+       //création d'un tableau qui affichera sous forme de liste
+       //ligne : cours (taille)
+       //colonne : les détails (entêtes) 
+       Object[][] table = new Object[taille][6];
         
-        List<Seance> liste = user.ListeSeances().getByUtilisateurAtDate(user.ListeUtilisateurs().getByID(1709), "2020-05-26");
-        int taille = liste.size();
-        
-        Object[][] table = new Object[6][taille];
-        
-        int i=0;
+       int i=0;
         
         for (Seance s : user.ListeSeances().getByUtilisateurAtDate(user.ListeUtilisateurs().getByID(1709), "2020-05-26")) 
         {
             System.out.println(s);
-            table[0][i]=s.getDebut();
-            table[1][i]=s.getFin();
-            table[2][i]=s.getCours();
-            table[3][i]=s.getEnseignants();
-            table[4][i]=s.getGroupes();
-            table[5][i]=s.getSalles();
-            table[6][i]=s.getType();
+            table[i][0]=s.getDebut();
+            table[i][1]=s.getFin();
+            table[i][2]=s.getCours();
+            table[i][3]=s.getEnseignants();
+            table[i][4]=s.getGroupes();
+            table[i][5]=s.getSalles();
+            table[i][6]=s.getType();
             i++;
         }
         
