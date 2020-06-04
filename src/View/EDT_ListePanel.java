2020f2a1066b20a555ae89cb25e7797580f813a4
LeonieCdt <leonie.zoi@hotmail.fr>
2020-06-04 12:08:12 +0200

    Merge origin/master
    Conflicts:
    	src/Utilitaires/path.java

--- src/View/EDT_ListePanel.java
+++ src/View/EDT_ListePanel.java
@@ -15,6 +15,12 @@ import javax.swing.BorderFactory;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import Controllers.Controller;
+import java.awt.GridBagLayout;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import javax.swing.JPanel;
 
 /**
  *
@@ -44,11 +50,11 @@ public class EDT_ListePanel extends Panel
         
         //JTabbedPane tabbedPanes = new JTabbedPane();
 
-//        JPanel panel1 = new JPanel();
-//        panel1.setLayout(new GridBagLayout());
+        JPanel panel1 = new JPanel();
+        panel1.setLayout(new GridBagLayout());
        
        //on récupère une liste avec toutes les séances qui se passe pendant une journée.
-       List<Seance> liste = user.ListeSeances().getByUtilisateurAtDate(user.ListeUtilisateurs().getByID(1709), new Date(new java.util.Date().getTime()));
+       List<Seance> liste = user.ListeSeances().getByUtilisateurAtWeek(user.getUtilisateurConnecte(), Integer.parseInt( new SimpleDateFormat("w").format(new java.util.Date()) ));//user.ListeSeances().getByUtilisateurAtDate(user.ListeUtilisateurs().getByID(1709), new Date(new java.util.Date().getTime()));
        //on récupère la taille pour savoir le nombre de cours en une journée et pouvoir ensuite passer au jour d'après.
        int taille = liste.size();
        //création d'un tableau qui affichera sous forme de liste
@@ -56,25 +62,47 @@ public class EDT_ListePanel extends Panel
        //colonne : les détails (entêtes) 
        Object[][] table = new Object[7][taille];
         
-      int i =0;
+       int i =0;
+       
+       
         
-        for (Seance s : user.ListeSeances().getByUtilisateurAtDate(user.ListeUtilisateurs().getByID(1709), new Date(new java.util.Date().getTime()))) 
-        {
+       //for (Seance s : user.ListeSeances().getByUtilisateurAtDate(user.ListeUtilisateurs().getByID(1709), new Date(new java.util.Date().getTime()))) 
+       for(Seance s : user.ListeSeances().getByUtilisateurAtWeek(user.getUtilisateurConnecte(), Integer.parseInt( new SimpleDateFormat("w").format(new java.util.Date()) )))
+       {
+           
+            System.out.println("tata 1");
             System.out.println(s);
-            table[0][i]=s.getDebut();
-            table[1][i]=s.getFin();
-            table[2][i]=s.getCours();
-            table[3][i]=s.getEnseignants();
-            table[4][i]=s.getGroupes();
-            table[5][i]=s.getSalles();
-            table[6][i]=s.getType();
+//            System.out.println("tata 2");
+//            //table[0][i]=s.getDebut();
+//            System.out.println("tata 3");
+//            //table[1][i]=s.getFin();
+//            System.out.println("tata 4");
+//            table[2][i]=s.getCours();
+//            System.out.println("tata 5");
+//            table[3][i]=s.getEnseignants();
+//            System.out.println("tata 6");
+//            table[4][i]=s.getGroupes();
+//            System.out.println("tata 7");
+//            table[5][i]=s.getSalles();
+//            System.out.println("tata 8");
+//            table[6][i]=s.getType();
+//            System.out.println("tata 9");
             i++;
         }
        
-        String[] entetes={"Heure Début", "Heure Fin", "Cours", "Enseignant", "Groupe", "Salle", "Type de Cours"};
-        JTable tableau= new JTable(table, entetes);
+       String[] entetes={"Heure Début", "Heure Fin", "Cours", "Enseignant", "Groupe", "Salle", "Type de Cours"};
+       JTable tableau= new JTable(table, entetes);
        
+       //JScrollPane scroll = new JScrollPane(tableau);
+        
+        c.gridx = 0;
+        c.gridy = 3;
+        
+        //scroll.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+        //scroll.setBackground(Color.white);
+        //this.add(c);
        
+       //panel1.add(this);
         
         //this.add(tabbedPanes, c);
         
